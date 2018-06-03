44c662ab480ce4ec553bb8021099203a900d5e0a
BaoyuLi <libaoyuxd@163.com>
2018-06-03 22:42:24 +0800

    Remove useless writing operation

--- read_from_file.m
+++ read_from_file.m
@@ -1,9 +1,8 @@
 function [ data ] = read_from_file( filename )
-%READ_FROM_FILE 此处显示有关此函数的摘要
-%   此处显示详细说明
+%READ_FROM_FILE 姝ゅ鏄剧ず鏈夊叧姝ゅ嚱鏁扮殑鎽樿
+%   姝ゅ鏄剧ず璇︾粏璇存槑
     fid = fopen(filename,'r','n','UTF-8');
     data = fread(fid)';
     data = native2unicode(data,'UTF-8');
-    write_to_file('result.txt',data);
+    #write_to_file('result.txt',data);
 end
-
